28474b07f65e6a1fd63522f1152f44f48ac8b183
Yang Liu <istef.liu@gmail.com>
2014-09-30 21:48:46 -0700

    Fix a bug in for loop

--- smb-client.sh
+++ smb-client.sh
@@ -7,7 +7,7 @@ targets=()
 num=1
 credentials_file_base="/root/smbcredentials"
 
-for mnt in "${mounts[@]}"; do
+for mnt in $mounts; do
 
   # parse smb url
   if [[ $mnt == smb://* ]]; then
@@ -27,8 +27,8 @@ for mnt in "${mounts[@]}"; do
     # test if we need credentials
     usrpwd=$(echo $mnt | awk -F '@' '{print $1}')
     if [ "$usrpwd" != "$mnt" ]; then
-      echo "username=$(echo $usrpwd | awk -F ':' '{print $1}')" > "${credentials_file_base}${num}"
-      echo "password=$(echo $usrpwd | awk -F ':' '{print $2}')" >> "${credentials_file_base}${num}"
+      # echo "username=$(echo $usrpwd | awk -F ':' '{print $1}')" > "${credentials_file_base}${num}"
+      # echo "password=$(echo $usrpwd | awk -F ':' '{print $2}')" >> "${credentials_file_base}${num}"
       credentials=",credentials=${credentials_file_base}${num}"
       mnt=$(echo $mnt | awk -F '@' '{print $2}')
     else
@@ -38,13 +38,17 @@ for mnt in "${mounts[@]}"; do
     # extract server/path & target
     src=$(echo $mnt | awk -F ':' '{print $1}')
     target=$(echo $mnt | awk -F ':' '{print $2}')
+    if [[ $target == /* ]]; then
+      target=${target:1}
+    fi
+    target="/mnt/samba/${target}"
     targets+=("$target")
 
     # do mounting
     mkdir -p $target
 
     echo "[DEBUG] Try to mount samba share: mount -t cifs -o file_mode=0644,dir_mode=0775${credentials}${domain} //${src} ${target}"
-    mount -t cifs -o file_mode=0644,dir_mode=0775${credentials}${domain} //${src} ${target}
+    # mount -t cifs -o file_mode=0644,dir_mode=0775${credentials}${domain} //${src} ${target}
 
     num=$((num+1))
   fi
